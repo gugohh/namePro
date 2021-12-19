7e5c075dae4fd95a458b6fef49043519b23c72eb
gugohh <oohhgug@gmail.com>
2021-12-19 18:27:16 +0100

    final commmit

--- src/store/types.ts
+++ src/store/types.ts
@@ -5,7 +5,7 @@ export const DELETE_LIST = 'DELETE_LIST';
 export const UPDATE_LIST = 'UPDATE_LIST';
 export const SET_LISTID_TO_DELETE = 'SET_LISTID_TO_DELETE';
 export const SET_LIST_TO_EDIT = 'SET_LIST_TO_EDIT';
-export const SET_SELECTED_LIST = 'SET_SELECTED_LIST'; 
+export const SET_SELECTED_LIST = 'SET_SELECTED_LIST';
 
 export const ADD_TASK = 'ADD_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
@@ -17,147 +17,144 @@ export const UNSET_TASK_TO_EDIT = 'UNSET_TASK_TO_EDIT';
 
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 
-
-// Creat interfacis fot Task and List
 export interface Task {
-    name: string;
-    id: string;
-    completed: boolean;
-  }
-  
-  export interface List {
-    name: string;
-    id: string;
-    tasks: Task[];
-  }
-  
-  export interface Lists {
-    [id: string]: List
-  }
+  name: string;
+  id: string;
+  completed: boolean;
+}
 
-//actions
+export interface List {
+  name: string;
+  id: string;
+  tasks: Task[];
+}
 
+export interface Lists {
+  [id: string]: List
+}
+
+// Actions
 interface AddListAction {
-    type: typeof ADD_LIST;
-    payload: List;
-  } 
-  
-  interface GetListsAction {
-    type: typeof GET_LISTS;
+  type: typeof ADD_LIST;
+  payload: List;
+} 
+
+interface GetListsAction {
+  type: typeof GET_LISTS;
+}
+
+interface GetListByIdAction {
+  type: typeof GET_LIST_BY_ID;
+  payload: string;
+}
+
+interface SetListIdToDeleteAction {
+  type: typeof SET_LISTID_TO_DELETE;
+  payload: string;
+}
+
+interface SetListToEditAction {
+  type: typeof SET_LIST_TO_EDIT;
+  payload: string;
+}
+
+interface DeleteListAction {
+  type: typeof DELETE_LIST;
+  payload: string;
+}
+
+interface UpdateListAction {
+  type: typeof UPDATE_LIST;
+  payload: {
+    id: string;
+    name: string;
   }
-  
-  interface GetListByIdAction {
-    type: typeof GET_LIST_BY_ID;
-    payload: string;
+}
+
+interface SetSelectedListAction {
+  type: typeof SET_SELECTED_LIST;
+  payload: string;
+}
+
+interface AddTaskAction {
+  type: typeof ADD_TASK;
+  payload: {
+    task: Task;
+    list: List;
   }
-  
-  interface SetListIdToDeleteAction {
-    type: typeof SET_LISTID_TO_DELETE;
-    payload: string;
+}
+
+interface DeleteTaskAction {
+  type: typeof DELETE_TASK;
+  payload: {
+    task: Task;
+    list: List;
   }
-  
-  interface SetListToEditAction {
-    type: typeof SET_LIST_TO_EDIT;
-    payload: string;
+}
+
+interface SetTaskToDeleteAction {
+  type: typeof SET_TASK_TO_DELETE;
+  payload: {
+    task: Task;
+    list: List;
   }
-  
-  interface DeleteListAction {
-    type: typeof DELETE_LIST;
-    payload: string;
+}
+
+interface UnsetTaskToDeleteAction {
+  type: typeof UNSET_TASK_TO_DELETE;
+}
+
+interface EditTaskAction {
+  type: typeof UPDATE_TASK;
+  payload: {
+    taskId: string;
+    taskName: string;
+    taskState: boolean;
+    list: List;
   }
-  
-  interface UpdateListAction {
-    type: typeof UPDATE_LIST;
-    payload: {
-      id: string;
-      name: string;
-    }
+}
+
+interface SetTaskToEditAction {
+  type: typeof SET_TASK_TO_EDIT;
+  payload: {
+    task: Task;
+    list: List;
   }
-  
-  interface SetSelectedListAction {
-    type: typeof SET_SELECTED_LIST;
-    payload: string;
-  }
-  
-  interface AddTaskAction {
-    type: typeof ADD_TASK;
-    payload: {
-      task: Task;
-      list: List;
-    }
-  }
-  
-  interface DeleteTaskAction {
-    type: typeof DELETE_TASK;
-    payload: {
-      task: Task;
-      list: List;
-    }
-  }
-  
-  interface SetTaskToDeleteAction {
-    type: typeof SET_TASK_TO_DELETE;
-    payload: {
-      task: Task;
-      list: List;
-    }
-  }
-  
-  interface UnsetTaskToDeleteAction {
-    type: typeof UNSET_TASK_TO_DELETE;
-  }
-  
-  interface EditTaskAction {
-    type: typeof UPDATE_TASK;
-    payload: {
-      taskId: string;
-      taskName: string;
-      taskState: boolean;
-      list: List;
-    }
-  }
-  
-  interface SetTaskToEditAction {
-    type: typeof SET_TASK_TO_EDIT;
-    payload: {
-      task: Task;
-      list: List;
-    }
-  }
-  
-  interface UnsetTaskToEditAction {
-    type: typeof UNSET_TASK_TO_EDIT;
-  }
-  
-  interface SetNotificationAction {
-    type: typeof SET_NOTIFICATION;
-    payload: {
-      msg: string;
-      type: string;
-    }
-  }
-  
-  export type ListsAction = AddListAction | GetListsAction | GetListByIdAction | SetListIdToDeleteAction | SetListToEditAction | DeleteListAction | UpdateListAction | SetSelectedListAction | AddTaskAction | DeleteTaskAction | SetTaskToDeleteAction | UnsetTaskToDeleteAction | EditTaskAction | SetTaskToEditAction | UnsetTaskToEditAction;
-  
-  export type NotificationAction = SetNotificationAction;
-  
-  export interface ListState {
-    lists: Lists;
-    listIdToDelete: string;
-    listToEdit: List | null;
-    listById: List | null;
-    selectedList: List | null;
-    taskToDelete: {
-      task: Task;
-      list: List;
-    } | null;
-    taskToEdit: {
-      task: Task;
-      list: List;
-    } | null;
-  }
-  
-  export interface NotificationState {
-    message: string;
+}
+
+interface UnsetTaskToEditAction {
+  type: typeof UNSET_TASK_TO_EDIT;
+}
+
+interface SetNotificationAction {
+  type: typeof SET_NOTIFICATION;
+  payload: {
+    msg: string;
     type: string;
   }
+}
+
+export type ListsAction = AddListAction | GetListsAction | GetListByIdAction | SetListIdToDeleteAction | SetListToEditAction | DeleteListAction | UpdateListAction | SetSelectedListAction | AddTaskAction | DeleteTaskAction | SetTaskToDeleteAction | UnsetTaskToDeleteAction | EditTaskAction | SetTaskToEditAction | UnsetTaskToEditAction;
+
+export type NotificationAction = SetNotificationAction;
+
+export interface ListState {
+  lists: Lists;
+  listIdToDelete: string;
+  listToEdit: List | null;
+  listById: List | null;
+  selectedList: List | null;
+  taskToDelete: {
+    task: Task;
+    list: List;
+  } | null;
+  taskToEdit: {
+    task: Task;
+    list: List;
+  } | null;
+}
+
+export interface NotificationState {
+  message: string;
+  type: string;
+}
